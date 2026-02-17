a40fe76669374a53f02a6fdf93d94bd2cfe9b8ce
Leo Broue <leobroue@hotmail.fr>
2026-02-17 20:57:13 +0100

    norming open_window, init_data, and main.c

--- srcs/render/init_data.c
+++ srcs/render/init_data.c
@@ -6,13 +6,13 @@
 /*   By: lebroue <lebroue@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/16 18:35:42 by lebroue           #+#    #+#             */
-/*   Updated: 2026/02/16 19:02:37 by lebroue          ###   ########.fr       */
+/*   Updated: 2026/02/17 20:37:26 by lebroue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-void init_data_map(t_game *game, t_file_data *data)
+void	init_data_map(t_game *game, t_file_data *data)
 {
 	game->data = data;
 	game->mlx = NULL;
@@ -21,14 +21,12 @@ void init_data_map(t_game *game, t_file_data *data)
 	game->img_data = NULL;
 }
 
-/* Initialiser la position du joueur (conversion int vers double
-		+ 0.5 pour centrer) */
-void init_player_position(t_game *game, t_file_data *data)
+void	init_player_position(t_game *game, t_file_data *data)
 {
 	game->player_x = data->player_x + 0.5;
 	game->player_y = data->player_y + 0.5;
 }
-void init_player_direction_north_south(t_game *game, t_file_data *data)
+void	init_player_direction_north_south(t_game *game, t_file_data *data)
 {
 	if (data->player_dir == 'N')
 	{
@@ -46,9 +44,8 @@ void init_player_direction_north_south(t_game *game, t_file_data *data)
 	}
 }
 
-void init_player_direction_east_west(t_game *game, t_file_data *data)
+void	init_player_direction_east_west(t_game *game, t_file_data *data)
 {
-	
 	if (data->player_dir == 'E')
 	{
 		game->player_dir_x = 1;
@@ -65,13 +62,13 @@ void init_player_direction_east_west(t_game *game, t_file_data *data)
 	}
 }
 
-void init_player_direction(t_game *game, t_file_data *data)
+void	init_player_direction(t_game *game, t_file_data *data)
 {
 	init_player_direction_north_south(game, data);
 	init_player_direction_north_south(game, data);
 }
 
-void init_key_press_state(t_game *game)
+void	init_key_press_state(t_game *game)
 {
 	memset(game->keys, 0, sizeof(game->keys));
 }
--- srcs/render/open_window.c
+++ srcs/render/open_window.c
@@ -6,51 +6,48 @@
 /*   By: lebroue <lebroue@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 15:58:55 by lebroue           #+#    #+#             */
-/*   Updated: 2026/02/17 15:59:27 by lebroue          ###   ########.fr       */
+/*   Updated: 2026/02/17 20:55:14 by lebroue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
+int	init_mlx_and_window(t_game *game)
+{
+	game->mlx = mlx_init();
+	if (!game->mlx)
+		return (printf("Failed to initialize MLX\n"), 1);
+	game->win = mlx_new_window(game->mlx, 800, 600, "Cub3D");
+	if (!game->win)
+		return (printf("Failed to create window\n"), 1);
+	game->img = mlx_new_image(game->mlx, 800, 600);
+	if (!game->img)
+		return (printf("Failed to create image\n"), 1);
+	game->img_data = mlx_get_data_addr(game->img,
+			&game->bpp, &game->line_len, &game->endian);
+	return (0);
+}
+void	setup_hooks(t_game *game)
+{
+	mlx_hook(game->win, 2, 1L << 0, key_press, game);
+	mlx_hook(game->win, 3, 1L << 1, key_release, game);
+	mlx_hook(game->win, 17, 0L, close_window, game);
+	mlx_hook(game->win, 15, 1L << 16, minimize_window, NULL);
+	mlx_hook(game->win, 22, 1L << 18, maximize_window, NULL);
+	mlx_loop_hook(game->mlx, (int (*)(void *))render, game);
+}
 int	ft_open_window(t_file_data *data)
 {
 	t_game	game;
 
 	init_game(&game, data);
-	game.mlx = mlx_init();
-	if (!game.mlx)
-	{
-		printf("Failed to initialize MLX\n");
+	if (init_mlx_and_window(&game))
 		return (1);
-	}
-	game.win = mlx_new_window(game.mlx, 800, 600, "Cub3D");
-	if (!game.win)
-	{
-		printf("Failed to create window\n");
-		return (1);
-	}
-	// Creer l'image pour le double buffering
-	game.img = mlx_new_image(game.mlx, 800, 600);
-	if (!game.img)
-	{
-		printf("Failed to create image\n");
-		return (1);
-	}
-	game.img_data = mlx_get_data_addr(game.img, &game.bpp, &game.line_len,
-			&game.endian);
-	// Charger les textures
 	load_textures(&game, data);
-	// Configurer les hooks pour les événements
-	mlx_hook(game.win, 2, 1L << 0, key_press, &game);   // Touche pressée
-	mlx_hook(game.win, 3, 1L << 1, key_release, &game); // Touche relâchée
-	mlx_hook(game.win, 17, 0L, close_window, &game);
-	// Fermeture fenêtre (croix)
-	mlx_hook(game.win, 15, 1L << 16, minimize_window, NULL); // Réduction
-	mlx_hook(game.win, 22, 1L << 18, maximize_window, NULL);
-	// Redimensionnement
-	// Configurer la boucle de rendu
-	mlx_loop_hook(game.mlx, (int (*)(void *))render, &game);
+	setup_hooks(&game);
 	mlx_loop(game.mlx);
 	return (0);
 }
+
+
 
