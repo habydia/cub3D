d007f51aca2bb0e705b62bf88905ba1eadb469a3
hadia <hadia@student.42lyon.fr>
2026-02-17 19:31:58 +0100

    refractor parsing

--- srcs/parsing/parse_colors.c
+++ srcs/parsing/parse_colors.c
@@ -3,92 +3,104 @@
 /*                                                        :::      ::::::::   */
 /*   parse_colors.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hadia <Hadia@student.42lyon.fr>            +#+  +:+       +#+        */
+/*   By: hadia <hadia@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 12:18:02 by hadia             #+#    #+#             */
-/*   Updated: 2026/02/15 15:42:55 by hadia            ###   ########.fr       */
+/*   Updated: 2026/02/17 19:30:53 by hadia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-int process_colors(char *line, t_file_data *data)
+int	process_colors(char *line, t_file_data *data)
 {
-    if (ft_strncmp(line, "F ", 2) == 0)
-    {
-        if (parse_rgb(line + 2, data->floor_color) == 0)
-        {
-            data->floor_set = 1;
-            return 0;
-        }
-        return -1;
-    }
-    else if (ft_strncmp(line, "C ", 2) == 0)
-    {
-        if (parse_rgb(line + 2, data->ceiling_color) == 0)
-        {
-            data->ceiling_set = 1;
-            return 0;
-        }
-        return -1;
-    }
-    return 1;  // Pas une couleur
+	if (ft_strncmp(line, "F ", 2) == 0)
+	{
+		if (parse_rgb(line + 2, data->floor_color) == 0)
+		{
+			data->floor_set = 1;
+			return (0);
+		}
+		return (-1);
+	}
+	else if (ft_strncmp(line, "C ", 2) == 0)
+	{
+		if (parse_rgb(line + 2, data->ceiling_color) == 0)
+		{
+			data->ceiling_set = 1;
+			return (0);
+		}
+		return (-1);
+	}
+	return (1); // Pas une couleur
 }
 
+static int	parse_coma(char *str)
+{
+	int	i;
+	int	comma_count;
+
+	i = 0;
+	comma_count = 0;
+	while (str[i])
+	{
+		if (str[i] == ',')
+		{
+			comma_count++;
+			if (i == 0 || str[i + 1] == ',' || str[i + 1] == '\0' || (i > 0
+					&& str[i - 1] == ','))
+				return (-1); // Virgule en début, fin, ou consécutive
+		}
+		i++;
+	}
+	if (comma_count != 2)
+		return (-1); // Pas exactement 2 virgules
+	return (0);
+}
 
 // Fonction pour parser RGB
-int parse_rgb(char *str, int color[3])
+int	parse_rgb(char *str, int color[3])
 {
-    int i = 0;
-    int comma_count = 0;
-    int start = 0;
-    int j = 0;
+	int		start;
+	int		j;
+	int		i;
+	char	*num_str;
 
-    // Compter les virgules et vérifier qu'il n'y en a pas de consécutives ou en début/fin
-    while (str[i])
-    {
-        if (str[i] == ',')
-        {
-            comma_count++;
-            if (i == 0 || str[i + 1] == ',' || str[i + 1] == '\0' || (i > 0 && str[i - 1] == ','))
-                return -1;  // Virgule en début, fin, ou consécutive
-        }
-        i++;
-    }
-    if (comma_count != 2)
-        return -1;  // Pas exactement 2 virgules
-
-    // Maintenant parser les valeurs
-    i = 0;
-    j = 0;
-    while (str[i] && j < 3)
-    {
-        if (str[i] == ',')
-        {
-            // Extraire la valeur de start à i-1
-            char *num_str = ft_substr(str, start, i - start);
-            if (!num_str)
-                return -1;
-            color[j] = ft_atoi(num_str);
-            free(num_str);
-            if (color[j] < 0 || color[j] > 255)
-                return -1;
-            start = i + 1;
-            j++;
-        }
-        i++;
-    }
-    // Dernière valeur
-    if (j < 3)
-    {
-        char *num_str = ft_substr(str, start, i - start);
-        if (!num_str)
-            return -1;
-        color[j] = ft_atoi(num_str);
-        free(num_str);
-        if (color[j] < 0 || color[j] > 255)
-            return -1;
-    }
-
-    return 0;
+	start = 0;
+	j = 0;
+	i = 0;
+	if (parse_coma(str) == -1)
+		return (-1);
+	// Maintenant parser les valeurs
+	i = 0;
+	j = 0;
+	while (str[i] && j < 3)
+	{
+		if (str[i] == ',')
+		{
+			// Extraire la valeur de start à i-1
+			num_str = ft_substr(str, start, i - start);
+			if (!num_str)
+				return (-1);
+			color[j] = ft_atoi(num_str);
+			free(num_str);
+			if (color[j] < 0 || color[j] > 255)
+				return (-1);
+			start = i + 1;
+			j++;
+		}
+		i++;
+	}
+	// Dernière valeur
+	if (j < 3)
+	{
+		num_str = ft_substr(str, start, i - start);
+		if (!num_str)
+			return (-1);
+		color[j] = ft_atoi(num_str);
+		free(num_str);
+		if (color[j] < 0 || color[j] > 255)
+			return (-1);
+	}
+	return (0);
 }
--- srcs/parsing/parse_textures.c
+++ srcs/parsing/parse_textures.c
@@ -3,91 +3,114 @@
 /*                                                        :::      ::::::::   */
 /*   parse_textures.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: hadia <Hadia@student.42lyon.fr>            +#+  +:+       +#+        */
+/*   By: hadia <hadia@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/10 12:19:20 by hadia             #+#    #+#             */
-/*   Updated: 2026/02/15 14:32:41 by hadia            ###   ########.fr       */
+/*   Updated: 2026/02/17 18:29:33 by hadia            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-int process_textures(char *line, t_file_data *data)
+
+//ok norme
+// refracorer pour éviter les répétitions
+
+static int	north_texture(t_file_data *data, char *line)
 {
-    if (ft_strncmp(line, "NO ", 3) == 0)
-    {
-        if (data->north_texture) 
-        {
-            printf("Error\nDuplicate NO texture\n");
-            return -1;
-        }
-        if (extract_texture(line + 3, &data->north_texture) == 0)
-        {
-            data->north_set = 1;
-            return 0;
-        }
-        return -1;
-    }
-    else if (ft_strncmp(line, "SO ", 3) == 0)
-    {
-        if (data->south_texture) 
-        {
-            printf("Error\nDuplicate SO texture\n");
-            return -1;
-        }
-        if (extract_texture(line + 3, &data->south_texture) == 0)
-        {
-            data->south_set = 1;
-            return 0;
-        }
-        return -1;
-    }
-    else if (ft_strncmp(line, "WE ", 3) == 0)
-    {
-        if (data->west_texture) 
-        {
-            printf("Error\nDuplicate WE texture\n");
-            return -1;
-        }
-        if (extract_texture(line + 3, &data->west_texture) == 0)
-        {
-            data->west_set = 1;
-            return 0;
-        }
-        return -1;
-    }
-    else if (ft_strncmp(line, "EA ", 3) == 0)
-    {
-        if (data->east_texture) 
-        {
-            printf("Error\nDuplicate EA texture\n");
-            return -1;
-        }
-        if (extract_texture(line + 3, &data->east_texture) == 0)
-        {
-            data->east_set = 1;
-            return 0;
-        }
-        return -1;
-    }
-    return 1;  // Pas une texture
+	if (data->north_texture)
+	{
+		printf("Error\nDuplicate NO texture\n");
+		return (-1);
+	}
+	if (extract_texture(line + 3, &data->north_texture) == 0)
+	{
+		data->north_set = 1;
+		return (0);
+	}
+	return (-1);
 }
 
-int extract_texture(char *path, char **texture)
+static int south_texture(t_file_data *data, char *line)
 {
-    
-    while (*path == ' ')
-        path++;
-    if (*path == '\0')
-        return -1;
-    int len = ft_strlen(path);
-    if (len < 4 || ft_strcmp(path + len - 4, ".xpm") != 0)
+    if (data->south_texture)
     {
-        printf("Error\nTexture file must have .xpm extension\n");
-        return -1;
+        printf("Error\nDuplicate SO texture\n");
+        return (-1);
     }
-    *texture = ft_strdup(path);
-    if (!*texture)
-        return -1;
-    return 0;
+    if (extract_texture(line + 3, &data->south_texture) == 0)
+    {
+        data->south_set = 1;
+        return (0);
+    }
+    return (-1);
+}
+
+static  int west_texture(t_file_data *data, char *line)
+{
+    if (data->west_texture)
+    {
+        printf("Error\nDuplicate WE texture\n");
+        return (-1);
+    }
+    if (extract_texture(line + 3, &data->west_texture) == 0)
+    {
+        data->west_set = 1;
+        return (0);
+    }
+    return (-1);
+}
+
+static int east_texture(t_file_data *data, char *line)
+{
+    if (data->east_texture)
+    {
+        printf("Error\nDuplicate EA texture\n");
+        return (-1);
+    }
+    if (extract_texture(line + 3, &data->east_texture) == 0)
+    {
+        data->east_set = 1;
+        return (0);
+    }
+    return (-1);
+}
+
+int	process_textures(char *line, t_file_data *data)
+{
+	if (ft_strncmp(line, "NO ", 3) == 0)
+	{
+		return (north_texture(data, line));
+	}
+	else if (ft_strncmp(line, "SO ", 3) == 0)
+	{
+		return (south_texture(data, line));
+	}
+	else if (ft_strncmp(line, "WE ", 3) == 0)
+	{
+        return (west_texture(data, line));
+	}
+	else if (ft_strncmp(line, "EA ", 3) == 0)
+	{
+        return (east_texture(data, line));
+	}
+	return (1); // Pas une texture
+}
+
+int	extract_texture(char *path, char **texture)
+{
+	while (*path == ' ')
+		path++;
+	if (*path == '\0')
+		return (-1);
+	int len = ft_strlen(path);
+	if (len < 4 || ft_strcmp(path + len - 4, ".xpm") != 0)
+	{
+		printf("Error\nTexture file must have .xpm extension\n");
+		return (-1);
+	}
+	*texture = ft_strdup(path);
+	if (!*texture)
+		return -1;
+	return 0;
 }
