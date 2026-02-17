2bd01363438da765d6de3717d9ccfc35dcf9b748
Leo Broue <leobroue@hotmail.fr>
2026-02-17 20:29:18 +0100

    managed and norme draw minimap

--- srcs/render/draw_minimap.c
+++ srcs/render/draw_minimap.c
@@ -6,67 +6,65 @@
 /*   By: lebroue <lebroue@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/17 15:48:42 by lebroue           #+#    #+#             */
-/*   Updated: 2026/02/17 18:30:33 by lebroue          ###   ########.fr       */
+/*   Updated: 2026/02/17 20:28:35 by lebroue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
 
-void	draw_minimap(t_game *game)
+int	get_minimap_color(char tile)
 {
-	int x_char_array;
-	int y_line_array;
-	int position_x;
-	int position_y;
-	int map_scale_for_pixel;
-	int color;
-
-	map_scale_for_pixel = 10; // taille d’une case en pixels
-	y_line_array = 0;
-
-	while (y_line_array < game->data->map_height)
-	{
-		x_char_array = 0;
-		while (x_char_array < game->data->map_width)
-		{
-			// Choix de la couleur
-			if (game->data->map[y_line_array][x_char_array] == '1')
-				color = 0xFFFFFF; // mur blanc
-			else
-				color = 0x000000; // vide noir
-
-			// Dessiner la case a la position (x, y) avec scale
-			position_y = 0;
-			while (position_y < map_scale_for_pixel)
-			{
-				position_x = 0;
-				while (position_x < map_scale_for_pixel)
-				{
-					
-					
-					put_pixel(game, x_char_array * map_scale_for_pixel + position_x, y_line_array * map_scale_for_pixel + position_y,
-						color);
-					position_x++;
-				}
-				position_y++;
-			}
-			x_char_array++;
-		}
-		y_line_array++;
-	}
-	draw_player_red_minimap(game, map_scale_for_pixel);
+	if (tile == '1')
+		return (0xFFFFFF);
+	return (0x000000);
 }
 
-void draw_player_red_minimap(t_game *game, int map_scale_for_pixel)
+void	draw_minimap_case(t_game *game, int x, int y, int scale, int color)
 {
-	
-	int player_px = (int)(game->player_x * map_scale_for_pixel);
-	int player_py = (int)(game->player_y * map_scale_for_pixel);
+	int	position_x;
+	int	position_y;
 
-	int i = -2;
+	position_y = 0;
+	while (position_y < scale)
+	{
+		position_x = 0;
+		while (position_x < scale)
+		{
+			put_pixel(game, x * scale + position_x, y * scale + position_y,
+				color);
+			position_x++;
+		}
+		position_y++;
+	}
+}
+
+void	draw_minimap_line(t_game *game, int y, int scale)
+{
+	int	x;
+	int	color;
+
+	x = 0;
+	while (x < game->data->map_width)
+	{
+		color = get_minimap_color(game->data->map[y][x]);
+		draw_minimap_case(game, x, y, scale, color);
+		x++;
+	}
+}
+
+void	draw_player_red_minimap(t_game *game, int map_scale_for_pixel)
+{
+	int	player_px;
+	int	player_py;
+	int	i;
+	int	j;
+
+	player_px = (int)(game->player_x * map_scale_for_pixel);
+	player_py = (int)(game->player_y * map_scale_for_pixel);
+	i = -2;
 	while (i <= 2)
 	{
-		int j = -2;
+		j = -2;
 		while (j <= 2)
 		{
 			put_pixel(game, player_px + i, player_py + j, 0xFF0000);
@@ -74,5 +72,18 @@ void draw_player_red_minimap(t_game *game, int map_scale_for_pixel)
 		}
 		i++;
 	}
-	
+}
+void	draw_minimap(t_game *game)
+{
+	int	y;
+	int	scale;
+
+	scale = 10;
+	y = 0;
+	while (y < game->data->map_height)
+	{
+		draw_minimap_line(game, y, scale);
+		y++;
+	}
+	draw_player_red_minimap(game, scale);
 }
