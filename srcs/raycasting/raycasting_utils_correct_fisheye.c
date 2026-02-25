393933cbbc71e66f62e31572d39209bb763c70ac
Leo Broue <leobroue@hotmail.fr>
2026-02-25 18:34:20 +0100

    fix: correct fisheye distance calculation with proper direction vectors

--- srcs/raycasting/raycasting_utils_correct_fisheye.c
+++ srcs/raycasting/raycasting_utils_correct_fisheye.c
@@ -6,7 +6,7 @@
 /*   By: lebroue <lebroue@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 18:00:00 by lebroue           #+#    #+#             */
-/*   Updated: 2026/02/25 18:28:49 by lebroue          ###   ########.fr       */
+/*   Updated: 2026/02/25 18:32:11 by lebroue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,8 @@ double	correct_fisheye_distance(t_game *game, t_ray *ray)
 
 	perp_distance = ray->ray_perpendicular_distance_to_wall;
 	angle_diff = atan2(ray->ray_dir_vector_y, ray->ray_dir_vector_x)
-		- atan2(game->player.direction_y, game->player.direction_x);
+		- atan2(game->player_vector_direction_y,
+			game->player_vector_direction_x);
 	cos_correction = cos(angle_diff);
 	if (cos_correction < 0.01)
 		cos_correction = 0.01;
