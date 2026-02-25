0b0a31bcf5c965fe96c7ad368f3a3eb2672f7c83
Leo Broue <leobroue@hotmail.fr>
2026-02-25 18:42:34 +0100

    refactor: fix wall distance calculation to meet 42 norms - break long assignment into multiple lines

--- srcs/raycasting/raycasting_utils_calculate_wall_distance.c
+++ srcs/raycasting/raycasting_utils_calculate_wall_distance.c
@@ -6,46 +6,59 @@
 /*   By: lebroue <lebroue@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/25 13:10:00 by lebroue           #+#    #+#             */
-/*   Updated: 2026/02/25 18:28:49 by lebroue          ###   ########.fr       */
+/*   Updated: 2026/02/25 18:40:20 by lebroue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
+#include <math.h>
+
+static double	calculate_ray_length_horizontal(t_game *game, t_ray *ray,
+		double offset)
+{
+	if (ray->ray_dir_vector_x != 0)
+		return ((ray->in_map_grid_cell_x - game->player_pos_x + offset)
+			/ ray->ray_dir_vector_x);
+	return (50.0);
+}
+
+static double	calculate_ray_length_vertical(t_game *game, t_ray *ray,
+		double offset)
+{
+	if (ray->ray_dir_vector_y != 0)
+		return ((ray->in_map_grid_cell_y - game->player_pos_y + offset)
+			/ ray->ray_dir_vector_y);
+	return (50.0);
+}
+
+static double	apply_perp_corr(t_game *game, t_ray *ray, double ray_length)
+{
+	double	ray_magnitude;
+	double	dot_product;
+
+	ray_magnitude = sqrt(ray->ray_dir_vector_x * ray->ray_dir_vector_x
+			+ ray->ray_dir_vector_y * ray->ray_dir_vector_y);
+	if (ray_magnitude <= 0)
+		return (ray_length);
+	dot_product = (game->player_vector_direction_x * ray->ray_dir_vector_x
+			+ game->player_vector_direction_y * ray->ray_dir_vector_y)
+		/ ray_magnitude;
+	return (ray_length * dot_product);
+}
 
 void	calculate_wall_distance(t_game *game, t_ray *ray)
 {
 	double	offset;
 	double	ray_length;
-	double	dot_product;
-	double	ray_length_magnitude;
 
-	offset = (1 - ray->ray_dda_grid_increment_direction_x) / 2;
 	if (ray->ray_wall_collision_is_vertical == 0)
-	{
-		if (ray->ray_dir_vector_x != 0)
-			ray_length = (ray->in_map_grid_cell_x - game->player_pos_x + offset)
-				/ ray->ray_dir_vector_x;
-		else
-			ray_length = 50.0;
-	}
+		offset = (1 - ray->ray_dda_grid_increment_direction_x) / 2;
 	else
-	{
 		offset = (1 - ray->ray_dda_grid_increment_direction_y) / 2;
-		if (ray->ray_dir_vector_y != 0)
-			ray_length = (ray->in_map_grid_cell_y - game->player_pos_y + offset)
-				/ ray->ray_dir_vector_y;
-		else
-			ray_length = 50.0;
-	}
-	ray_length_magnitude = sqrt(ray->ray_dir_vector_x * ray->ray_dir_vector_x
-			+ ray->ray_dir_vector_y * ray->ray_dir_vector_y);
-	if (ray_length_magnitude > 0)
-	{
-		dot_product = (game->player_vector_direction_x * ray->ray_dir_vector_x
-				+ game->player_vector_direction_y * ray->ray_dir_vector_y)
-			/ ray_length_magnitude;
-		ray->ray_perpendicular_distance_to_wall = ray_length * dot_product;
-	}
+	if (ray->ray_wall_collision_is_vertical == 0)
+		ray_length = calculate_ray_length_horizontal(game, ray, offset);
 	else
-		ray->ray_perpendicular_distance_to_wall = ray_length;
+		ray_length = calculate_ray_length_vertical(game, ray, offset);
+	ray->ray_perpendicular_distance_to_wall = apply_perp_corr(game, ray,
+			ray_length);
 }
