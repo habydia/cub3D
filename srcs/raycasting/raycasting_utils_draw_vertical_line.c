d3cfb5d90f6f3e2804508dbbbb7afa5ed5f86bab
Leo Broue <leobroue@hotmail.fr>
2026-02-26 16:42:07 +0100

    refactor: rename file to raycasting_utils_draw_vertical_line_utils_redner_wall_pixel_clumn

--- srcs/raycasting/raycasting_utils_draw_vertical_line.c
+++ srcs/raycasting/raycasting_utils_draw_vertical_line.c
@@ -6,31 +6,11 @@
 /*   By: lebroue <lebroue@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 01:00:00 by lebroue           #+#    #+#             */
-/*   Updated: 2026/02/26 16:36:37 by lebroue          ###   ########.fr       */
+/*   Updated: 2026/02/26 16:39:48 by lebroue          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/cub3d.h"
-
-static int	calculate_texture_y_coordinate(int tex_y, int tex_height)
-{
-	if (tex_y < 0)
-		tex_y = 0;
-	if (tex_y >= tex_height)
-		tex_y = tex_height - 1;
-	return (tex_y);
-}
-
-static unsigned int	get_texture_pixel_color(t_texture_data *texture, int tex_x,
-		int tex_y)
-{
-	if (texture->texture_pixel_buffer && tex_x >= 0
-		&& tex_x < texture->texture_width && tex_y >= 0
-		&& tex_y < texture->texture_height)
-		return (*(unsigned int *)(texture->texture_pixel_buffer + (tex_y
-					* texture->texture_bytes_per_scanline) + (tex_x * 4)));
-	return (0xFFFFFF);
-}
 
 static int	calculate_wall_column_height(t_game *game, t_ray *ray, int h)
 {
@@ -61,24 +41,6 @@ static void	calculate_texture_sampling_params(t_wall_column_render_params *param
 	params->texture_vertical_position = (params->screen_draw_start_y - h / 2
 			+ params->wall_column_pixel_height / 2)
 		* params->texture_sample_step;
-}
-
-void	render_wall_pixel_column(t_game *game, int x, int tex_x,
-		t_wall_column_render_params params)
-{
-	int	y;
-	int	tex_y;
-
-	y = params.screen_draw_start_y;
-	while (y <= params.screen_draw_end_y)
-	{
-		tex_y = calculate_texture_y_coordinate((int)params.texture_vertical_position,
-				params.texture->texture_height);
-		put_pixel(game, x, y, get_texture_pixel_color(params.texture, tex_x,
-				tex_y));
-		params.texture_vertical_position += params.texture_sample_step;
-		y++;
-	}
 }
 
 void	draw_wall_vertical_line(t_game *game, t_ray *ray, int x, int h)
---
+++ srcs/raycasting/raycasting_utils_draw_vertical_line_utils_redner_wall_pixel_clumn.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   raycasting_utils_draw_vertical_line_utils.c        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lebroue <lebroue@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/26 16:40:00 by lebroue           #+#    #+#             */
+/*   Updated: 2026/02/26 16:39:33 by lebroue          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/cub3d.h"
+
+static int	calculate_texture_y_coordinate(int tex_y, int tex_height)
+{
+	if (tex_y < 0)
+		tex_y = 0;
+	if (tex_y >= tex_height)
+		tex_y = tex_height - 1;
+	return (tex_y);
+}
+
+static unsigned int	get_texture_pixel_color(t_texture_data *texture, int tex_x,
+		int tex_y)
+{
+	if (texture->texture_pixel_buffer && tex_x >= 0
+		&& tex_x < texture->texture_width && tex_y >= 0
+		&& tex_y < texture->texture_height)
+		return (*(unsigned int *)(texture->texture_pixel_buffer + (tex_y
+					* texture->texture_bytes_per_scanline) + (tex_x * 4)));
+	return (0xFFFFFF);
+}
+
+void	render_wall_pixel_column(t_game *game, int x, int tex_x,
+		t_wall_column_render_params params)
+{
+	int	y;
+	int	tex_y;
+
+	y = params.screen_draw_start_y;
+	while (y <= params.screen_draw_end_y)
+	{
+		tex_y = calculate_texture_y_coordinate((int)params.texture_vertical_position,
+				params.texture->texture_height);
+		put_pixel(game, x, y, get_texture_pixel_color(params.texture, tex_x,
+				tex_y));
+		params.texture_vertical_position += params.texture_sample_step;
+		y++;
+	}
+}
